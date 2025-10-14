915ed53cc1cb3806043b1c2c8e309bf7cc8af92b
agussvn <neivagus@gmail.com>
2025-10-14 10:08:11 +0200

    https://github.com/agussvn/ejcortos

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("¿Cuántos bloques quieres picar? ");
+        int bloques = sc.nextInt();
+
+        for (int i = 1; i <= bloques; i++) {
+            System.out.println("Picando bloque " + i + "...");
+        }
+
+        System.out.println("¡Has picado " + bloques + " bloques!");
+
+        sc.close();
+    }
+}
